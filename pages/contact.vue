013f6c5ff026258a035209a21e790d9ee169ae4d
Benoit Martel <martel.b@gmail.com>
2020-10-27 13:55:12 -0400

    form 2

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="content">
-    <form name="contact" netlify>
+    <form name="contact" method="POST" data-netlify="true">
 		<input
 			class="form-control valid"
 			name="name"
@@ -11,6 +11,11 @@
 			placeholder="Nom"
 		/>
 		<input
+  class="hidden"
+  name="bot-field"
+  placeholder="Don't fill this out if you're a human"
+/>/>
+<input
 			class="form-control valid"
 			name="email"
 			id="email"
